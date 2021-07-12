c5be60180071f6bfbc2fe0d6a9da204f0df8e550
Tammo Ippen <tammo.ippen@posteo.de>
2021-07-12 21:25:30 +0200

    move to dots.zig

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ samples/dots: samples/dots.c
 	$(CC) -lzig-plotille -L$(PWD)/zig-out/lib samples/dots.c -o samples/dots
 
 gen-headers:
-	// do not forget to uncomment the `lib.emit_h = true;` part
+	# do not forget to uncomment the `lib.emit_h = true;` part
 	$(MAKE) clean
 	docker run -it --rm -v $(PWD):/app \
 				--platform linux/amd64 \
--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,2 @@
 // usingnamespace @import("./color.zig");
 usingnamespace @import("./dots.zig");
-
-// C API
-
-export fn dots_init() Dots {
-    return Dots.init();
-}
-export fn dots_str(self: Dots, buf: [*]u8, len: usize) u8 {
-    return self.str(buf[0..len]);
-}
-export fn dots_fill(self: *Dots) void {
-    return self.fill();
-}
-export fn dots_clear(self: *Dots) void {
-    return self.clear();
-}
-export fn dots_set(self: *Dots, x: u8, y: u8) void {
-    return self.set(x, y);
-}
-export fn dots_unset(self: *Dots, x: u8, y: u8) void {
-    return self.unset(x, y);
-}
