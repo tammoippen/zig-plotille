6464d53a5ce111ed979c98aacfea429196c2a07b
Tammo Ippen <tammo.ippen@posteo.de>
2021-07-12 12:36:32 +0200

    color struct

--- src/color.zig
+++ src/color.zig
@@ -8,9 +8,148 @@ const expect = testing.expect;
 
 const ColorMode = enum {
     names,
-    bytes,
+    lookup,
     rgb,
 };
+
+const Color = struct {
+    mode: ColorMode,
+    name: ?ColorName,
+    lookup: ?u8,
+    rgb: ?[3]u8,
+
+    pub fn by_name(name: ColorName) Color {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
+        return Color{
+            .mode = ColorMode.names,
+            .name = name,
+            .lookup = null,
+            .rgb = null,
+        };
+    }
+    pub fn by_lookup(idx: u8) Color {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+        // 0-  7:  standard colors (as in ESC [ 30–37 m)
+        // 8- 15:  high intensity colors (as in ESC [ 90–97 m)
+        // 16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
+        // 232-255:  grayscale from black to white in 24 steps
+        return Color{
+            .mode = ColorMode.lookup,
+            .name = null,
+            .lookup = idx,
+            .rgb = null,
+        };
+    }
+    pub fn by_rgb(r: u8, g: u8, b: u8) Color {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
+        //
+        return Color{
+            .mode = ColorMode.rgb,
+            .name = null,
+            .lookup = null,
+            .rgb = [_]u8{ r, g, b },
+        };
+    }
+    pub fn by_hsl(h: f64, s: f64, l: f64) !Color {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
+        // converts hsl values into rgb values.
+        try expect(h >= 0 and h <= 360);
+        try expect(s >= 0 and s <= 1);
+        try expect(l >= 0 and l <= 1);
+        var r: f64 = l;
+        var g: f64 = l;
+        var b: f64 = l;
+
+        if (s > 0) {
+            const q = if (l < 0.5) l * (1.0 + s) else l + s - l * s;
+            const p = 2 * l - q;
+            const h_ = h / 360.0;
+
+            r = hue_to_rgb(p, q, h_ + 1.0 / 3.0);
+            g = hue_to_rgb(p, q, h_);
+            b = hue_to_rgb(p, q, h_ - 1.0 / 3.0);
+        }
+
+        return Color{
+            .mode = ColorMode.rgb,
+            .name = null,
+            .lookup = null,
+            .rgb = [_]u8{ @floatToInt(u8, r * 255), @floatToInt(u8, g * 255), @floatToInt(u8, b * 255) },
+        };
+    }
+
+    fn hue_to_rgb(p: f64, q: f64, t: f64) f64 {
+        var t_ = t;
+        if (t < 0) {
+            t_ += 1.0;
+        }
+        if (t > 1) {
+            t_ -= 1.0;
+        }
+        if (t_ < 1.0 / 6.0) {
+            return p + (q - p) * 6.0 * t_;
+        }
+        if (t_ < 0.5) {
+            return q;
+        }
+        if (t_ < 2.0 / 3.0) {
+            return p + (q - p) * (2.0 / 3.0 - t_) * 6.0;
+        }
+        return p;
+    }
+};
+
+test "color by name" {
+    const c = Color.by_name(ColorName.bright_blue);
+
+    try expect(c.mode == ColorMode.names);
+    try expect(c.name == ColorName.bright_blue);
+}
+
+test "color by index" {
+    const c = Color.by_lookup(123);
+
+    try expect(c.mode == ColorMode.lookup);
+    try expect(c.lookup.? == 123);
+}
+
+test "color by rgb" {
+    const c = Color.by_rgb(255, 0, 0);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 255, 0, 0 })[0..]));
+}
+
+test "color by hsl red" {
+    const c = try Color.by_hsl(0, 1, 0.5);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 255, 0, 0 })[0..]));
+}
+test "color by hsl green" {
+    const c = try Color.by_hsl(120.0, 1.0, 0.5);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 0, 255, 0 })[0..]));
+}
+test "color by hsl blue" {
+    const c = try Color.by_hsl(240.0, 1.0, 0.5);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 0, 0, 255 })[0..]));
+}
+test "color by hsl white" {
+    const c = try Color.by_hsl(0, 0, 1);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 255, 255, 255 })[0..]));
+}
+test "color by hsl black" {
+    const c = try Color.by_hsl(0, 0, 0);
+
+    try expect(c.mode == ColorMode.rgb);
+    try expect(mem.eql(u8, c.rgb.?[0..], ([3]u8{ 0, 0, 0 })[0..]));
+}
 
 // Surround `text` with control characters for coloring
 //
@@ -127,6 +266,14 @@ const ColorName = enum(u8) {
     bright_magenta,
     bright_cyan,
     bright_white,
+    bright_black_old,
+    bright_red_old,
+    bright_green_old,
+    bright_yellow_old,
+    bright_blue_old,
+    bright_magenta_old,
+    bright_cyan_old,
+    bright_white_old,
 };
 // Foreground color codes
 const FGColors = [_][]const u8{
@@ -138,14 +285,22 @@ const FGColors = [_][]const u8{
     "35", // magenta
     "36", // cyan
     "37", // white
-    "1;30", // bright_black
-    "1;31", // bright_red
-    "1;32", // bright_green
-    "1;33", // bright_yellow
-    "1;34", // bright_blue
-    "1;35", // bright_magenta
-    "1;36", // bright_cyan
-    "1;37", // bright_white
+    "90", // bright_black
+    "91", // bright_red
+    "92", // bright_green
+    "93", // bright_yellow
+    "94", // bright_blue
+    "95", // bright_magenta
+    "96", // bright_cyan
+    "97", // bright_white
+    "1;30", // bright_black, old variant
+    "1;31", // bright_red, old variant
+    "1;32", // bright_green, old variant
+    "1;33", // bright_yellow, old variant
+    "1;34", // bright_blue, old variant
+    "1;35", // bright_magenta, old variant
+    "1;36", // bright_cyan, old variant
+    "1;37", // bright_white, old variant
 };
 
 // Background color codes
@@ -166,6 +321,14 @@ const BGColors = [_][]const u8{
     "105", // bright_magenta
     "106", // bright_cyan
     "107", // bright_white
+    "1;40", // bright_black, old variant
+    "1;41", // bright_red, old variant
+    "1;42", // bright_green, old variant
+    "1;43", // bright_yellow, old variant
+    "1;44", // bright_blue, old variant
+    "1;45", // bright_magenta, old variant
+    "1;46", // bright_cyan, old variant
+    "1;47", // bright_white, old variant
 };
 
 fn names(optional_fg: ?ColorName, optional_bg: ?ColorName, out: []u8) usize {
@@ -209,8 +372,8 @@ test "names with optional fg, bg" {
     try expect(mem.eql(u8, "\x1b[41m", buff[0..len]));
 
     len = names(ColorName.bright_green, ColorName.red, &buff);
-    try expect(len == 10);
-    try expect(mem.eql(u8, "\x1b[1;32;41m", buff[0..len]));
+    try expect(len == 8);
+    try expect(mem.eql(u8, "\x1b[92;41m", buff[0..len]));
 }
 
 test "color in names mode" {
@@ -229,6 +392,6 @@ test "color in names mode" {
     try expect(mem.eql(u8, "\x1b[41mSome text\x1b[0m", buff[0..len]));
 
     len = color_names("Some text", &buff, ColorName.bright_magenta, ColorName.red, false);
-    try expect(len == 23);
-    try expect(mem.eql(u8, "\x1b[1;35;41mSome text\x1b[0m", buff[0..len]));
+    try expect(len == 21);
+    try expect(mem.eql(u8, "\x1b[95;41mSome text\x1b[0m", buff[0..len]));
 }
