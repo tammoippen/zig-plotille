212af5e49a3416f52e0e33ea1f93fce930b1dbc6
Tammo Ippen <tammo.ippen@posteo.de>
2022-11-02 17:45:25 +0100

    fix example/test

--- examples/terminfo.zig
+++ examples/terminfo.zig
@@ -5,8 +5,11 @@ const plt = @import("zig-plotille");
 const TermInfo = plt.terminfo.TermInfo;
 
 pub fn main() !void {
-    // I use this example for testing, hence provide a testing allocator
-    try TermInfo.detect(std.testing.allocator);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    try TermInfo.detect(allocator);
     const info = TermInfo.get();
 
     const writer = std.io.getStdOut().writer();
